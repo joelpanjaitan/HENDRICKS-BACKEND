95404d94ca11bab995e5c4e222defe974d69b3f7
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-10-24 20:13:23 +0700

    update for port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log("Server is running on port: 3000");
+  console.log("Server is running on port: 5000");
 });
 
 app.get("/", (req, res) => {
